793be2255e3d20f56ef0f8f4a0d71cc6a00e70f5
Younusaliakash <younusaliakash8@gmail.comm>
2022-02-26 17:19:04 +0600

    Design done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import { BsFillStopFill, BsRecordCircleFill } from "react-icons/bs";
+import { IoMdDownload } from "react-icons/io";
+import "./App.css";
+
+let chunks = [];
 
 function App() {
+  let [stream, setStream] = useState(null) ;
+  let [audio, setAudio] = useState(null) ;
+  let [mixedStream, setMixedStream] = useState(null) ;
+  let [recorder, setrecorder] = useState(null) ;
+  
+
+  const videoFeedback = useRef();
+  const videoDownload = useRef();
+  const linkRef = useRef();
+
+
+    async function setupStream () {
+      try{
+        const _stream = await navigator.mediaDevices.getDisplayMedia({
+          video: true
+        })
+        setStream(_stream)
+
+        const _audio = await navigator.mediaDevices.getUserMedia({
+          audio: {
+            echoCancellation: true,
+            noiseSuppression: true,
+            sampleRate: 44100
+          }
+        })
+        setAudio(_audio)
+        setupVideoFeedBack();
+
+      }catch(e){
+        console.log("Error",e)
+      }
+    }
+
+    useEffect(() => {setMixedStream()}, [stream])
+
+  const setupVideoFeedBack = () => {
+    if(stream){
+      videoFeedback.current.srcObject = stream
+      videoFeedback.current.play()
+    }
+    else{
+      console.warn("No stream avialable")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen_recorder">
+      <h1 className="title">Webrec Screen Recorder</h1>
+      <div className="main_recorder_container">
+        <div className="preview">
+          {console.log(videoFeedback.current)}
+          {stream && <video
+            ref={videoFeedback}
+            autoPlay
+            className="main_recorder_preview"
+          ></video>}
+        </div>
+        <div className="action_btn">
+          <button onClick={setupStream}>
+            {" "}
+            <BsRecordCircleFill className="icon" />{" "}
+            <span className="btn_title">Start Recording</span>
+          </button>
+          <button >
+            {" "}
+            <BsFillStopFill className="icon" />{" "}
+            <span className="btn_title">Stop Recording</span>
+          </button>
+        </div>
+      </div>
+      <div className="recording_download_section">
+        <div className="preview_recorded_video">
+          <video className="download_preview" ref={videoDownload}></video>
+        </div>
+        <div className="download_btn">
+          <a ref={linkRef}>
+            {" "}
+            <IoMdDownload className="icon" />{" "}
+            <span className="btn_title">Download Video</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
